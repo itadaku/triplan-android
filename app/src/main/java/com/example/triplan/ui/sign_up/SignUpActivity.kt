28df0adb567f92d5e3974b9f408aa704aafe911b
Ryuki0222 <39976899+Ryuki0222@users.noreply.github.com>
2019-10-22 23:40:18 +0900

    新規ユーザの登録画面のデモ画面作成 (#4)

--- app/src/main/java/com/example/triplan/ui/sign_up/SignUpActivity.kt
+++ app/src/main/java/com/example/triplan/ui/sign_up/SignUpActivity.kt
@@ -3,13 +3,33 @@ package com.example.triplan.ui.sign_up
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.example.triplan.R
+import kotlinx.android.synthetic.main.activity_sing_up.*
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sing_up)
+
+        val nearestStationLineSpinnerAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            arrayOf("山手線", "京浜東北線", "中央線", "東海道線", "常磐線", "埼京線"))
+
+        nearestStationLineSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        signUpSpinnerNearestStationLine.adapter =nearestStationLineSpinnerAdapter
+
+        val nearestStationStationSpinnerAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            (1..10).map { "${it}駅" }.toTypedArray())
+        nearestStationStationSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        signUpSpinnerNearestStationStation.adapter = nearestStationStationSpinnerAdapter
+        signUpButton.setOnClickListener {
+
+        }
     }
 
     companion object {
@@ -18,4 +38,5 @@ class SignUpActivity : AppCompatActivity() {
             context.startActivity(intent)
         }
     }
+
 }
