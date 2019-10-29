5cc6bbc108900de6f57e5a29b8b8a70c762a64b3
Ryuki <ryuki@meuron.jp>
2019-10-30 06:37:58 +0900

    フラグメントとフラグメントの切り替えを実装

--- app/src/main/java/com/example/triplan/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/triplan/ui/main/MainActivity.kt
@@ -5,12 +5,69 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.triplan.R
+import com.example.triplan.ui.community.CommunityFragment
+import com.example.triplan.ui.now.NowFragment
+import com.example.triplan.ui.setting.SettingFragment
+import com.example.triplan.ui.suggest.SuggestFragment
+import com.example.triplan.ui.top.TopFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(mainHostFragmentLayout.id, TopFragment())
+            .commit()
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.main_navigation_top -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(mainHostFragmentLayout.id, TopFragment())
+                        .commit()
+                    return@setOnNavigationItemSelectedListener  true
+                }
+
+                R.id.main_navigation_suggest -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(mainHostFragmentLayout.id, SuggestFragment())
+                        .commit()
+                    return@setOnNavigationItemSelectedListener  true
+                }
+
+                R.id.main_navigation_now -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(mainHostFragmentLayout.id, NowFragment())
+                        .commit()
+                    return@setOnNavigationItemSelectedListener  true
+                }
+
+                R.id.main_navigation_community -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(mainHostFragmentLayout.id, CommunityFragment())
+                        .commit()
+                    return@setOnNavigationItemSelectedListener  true
+                }
+
+                R.id.main_navigation_setting -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(mainHostFragmentLayout.id, SettingFragment())
+                        .commit()
+                    return@setOnNavigationItemSelectedListener  true
+                }
+
+                else -> {
+                    return@setOnNavigationItemSelectedListener false
+                }
+            }
+        }
+
+//        val navController = findNavController(mainHostFragment.id)
+//        bottomNavigationView.setupWithNavController(navController)
+//        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 
     companion object {
---
+++ app/src/main/java/com/example/triplan/ui/suggest/SuggestFragment.kt
@@ -0,0 +1,18 @@
+package com.example.triplan.ui.suggest
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.triplan.R
+
+class SuggestFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_suggest, container, false)
+    }
+}
