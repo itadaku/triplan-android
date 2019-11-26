e0435d6a9d5273a579c0e126b354b2a9336ba4de
ryuji sanada <m15078@g.metro-cit.ac.jp>
2019-11-27 02:02:08 +0900

    「ご意見・ご要望」、「不具合報告」の通信処理実装

--- app/src/main/java/com/example/triplan/ui/setting_contact/SettingContactActivity.kt
+++ app/src/main/java/com/example/triplan/ui/setting_contact/SettingContactActivity.kt
@@ -6,16 +6,44 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.triplan.R
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModelProviders
+import com.example.triplan.api.model.Body.SettingBody
+import com.example.triplan.model.Setting
 import kotlinx.android.synthetic.main.activity_setting_contact.*
 
 class SettingContactActivity : AppCompatActivity() {
+    private lateinit var viewModel: SettingContactViewModel
+    private val setting = MutableLiveData<Setting>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting_contact)
 
+        viewModel = ViewModelProviders.of(this).get(SettingContactViewModel::class.java)
+
         settingContactBack.setOnClickListener {
             setResult(Activity.RESULT_OK)
             finish()
+        }
+
+        settingContactButton.setOnClickListener { view ->
+            view.isEnabled = false
+            val text = settingContactText.text.toString().trim()
+            if (text.isEmpty()) {
+                Toast.makeText(this, "文章を入力してください", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val settingContactBody = SettingBody(text)
+
+            viewModel.sendContentData(settingContactBody, {
+                Toast.makeText(this, "送信に成功しました", Toast.LENGTH_SHORT).show()
+            }, {response ->
+                Toast.makeText(this, response.errorMessage(this), Toast.LENGTH_SHORT).show()
+                view.isEnabled = true
+            })
         }
     }
 
--- app/src/main/java/com/example/triplan/ui/setting_feedback/SettingFeedbackActivity.kt
+++ app/src/main/java/com/example/triplan/ui/setting_feedback/SettingFeedbackActivity.kt
@@ -6,16 +6,44 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.triplan.R
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModelProviders
+import com.example.triplan.api.model.Body.SettingBody
+import com.example.triplan.model.Setting
 import kotlinx.android.synthetic.main.activity_setting_feedback.*
 
 class SettingFeedbackActivity : AppCompatActivity() {
+    private lateinit var viewModel: SettingFeedbackViewModel
+    private val setting = MutableLiveData<Setting>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting_feedback)
 
+        viewModel = ViewModelProviders.of(this).get(SettingFeedbackViewModel::class.java)
+
         settingFeedbackBack.setOnClickListener {
             setResult(Activity.RESULT_OK)
             finish()
+        }
+
+        settingFeedbackText.setOnClickListener { view ->
+            view.isEnabled = false
+            val text = settingFeedbackText.text.toString().trim()
+            if (text.isEmpty()) {
+                Toast.makeText(this, "文章を入力してください", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val settingFeedbackBody = SettingBody(text)
+
+            viewModel.sendFeedbackData(settingFeedbackBody, {
+                Toast.makeText(this, "送信に成功しました", Toast.LENGTH_SHORT).show()
+            }, {response ->
+                Toast.makeText(this, response.errorMessage(this), Toast.LENGTH_SHORT).show()
+                view.isEnabled = true
+            })
         }
     }
 
