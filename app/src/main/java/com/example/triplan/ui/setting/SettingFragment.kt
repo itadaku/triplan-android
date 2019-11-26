f0130d6d9632b1673b2b5548e37a0fb8748f5157
ryuji sanada <m15078@g.metro-cit.ac.jp>
2019-11-27 03:15:44 +0900

    RecyclerViewのアイテム別の処理の実装

--- app/src/main/java/com/example/triplan/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/example/triplan/ui/setting/SettingFragment.kt
@@ -6,7 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.triplan.R
+import com.example.triplan.ui.setting_contact.SettingContactActivity
+import com.example.triplan.ui.setting_feedback.SettingFeedbackActivity
 import kotlinx.android.synthetic.main.fragment_setting.*
+
+data class SettingLists(
+    val id: SettingListId,
+    val name: String
+)
+
+enum class SettingListId(val value: Int) {
+    EDIT_USER_INFORMATION(0),
+    CONTACT(1),
+    FEEDBACK(2),
+    TERMS_OF_SERVICE(3)
+}
 
 class SettingFragment: Fragment() {
 
@@ -20,9 +34,32 @@ class SettingFragment: Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        val settingLists = resources.getStringArray(R.array.setting_item_array_text)
-            .toList()
+
+        val settingLists = listOf<SettingLists>(
+            SettingLists(SettingListId.EDIT_USER_INFORMATION, "ユーザ情報編集"),
+            SettingLists(SettingListId.CONTACT, "ご意見・ご要望"),
+            SettingLists(SettingListId.FEEDBACK, "不具合報告"),
+            SettingLists(SettingListId.TERMS_OF_SERVICE, "利用規約")
+        )
         val settingRecyclerViewAdapter = SettingRecyclerViewAdapter(settingLists)
+        settingRecyclerViewAdapter.setOnItemClickListener(object : SettingRecyclerViewAdapter.OnItemClickListener {
+            override fun onClick(view: View, data: SettingLists) {
+                when(data.id) {
+                    SettingListId.EDIT_USER_INFORMATION -> {
+
+                    }
+                    SettingListId.CONTACT -> {
+                        SettingContactActivity.start(view.context)
+                    }
+                    SettingListId.FEEDBACK -> {
+                        SettingFeedbackActivity.start(view.context)
+                    }
+                    SettingListId.TERMS_OF_SERVICE -> {
+
+                    }
+                }
+            }
+        })
         settingRecyclerView.adapter = settingRecyclerViewAdapter
     }
 }
--- app/src/main/java/com/example/triplan/ui/setting/SettingRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/triplan/ui/setting/SettingRecyclerViewAdapter.kt
@@ -3,31 +3,43 @@ package com.example.triplan.ui.setting
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.triplan.R
 import com.example.triplan.lib.ViewHolder
 import com.example.triplan.lib.getSafety
 import com.example.triplan.ui.setting_contact.SettingContactActivity
+import com.example.triplan.ui.setting_feedback.SettingFeedbackActivity
 import kotlinx.android.synthetic.main.item_setting.view.*
 
 
 class SettingRecyclerViewAdapter(
-    private val settingLists: List<String>
+    private val settingLists: List<SettingLists>
 ): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    lateinit var listener: OnItemClickListener
+
     override fun getItemCount(): Int {
         return settingLists.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        setOnItemClickListener(listener)
         val inflater = LayoutInflater.from(parent.context)
         return ViewHolder.create(inflater, parent, R.layout.item_setting)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        holder.itemView.settingText.text = settingLists.getSafety(position)
+        holder.itemView.settingText.text = settingLists.getSafety(position)?.name
         holder.itemView.setOnClickListener {
-            val settingContact = SettingContactActivity
-            settingContact.start(it.context)
+            listener.onClick(it, settingLists[position])
         }
+    }
+
+    interface OnItemClickListener {
+        fun onClick(view: View, data: SettingLists)
+    }
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.listener = listener
     }
 }
